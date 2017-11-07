76450cd71079e51593914525f3c4747d15762bac
zhang-hai-jun <790052532@qq.com>
2017-11-07 15:09:50 +0800

    第一章源代码

---
+++ chapter 1/app/Card.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+import CheckList from './CheckList';
+
+class Card extends Component {
+    constructor(){
+        super();
+        this.state = {
+            showDetails:false
+        };
+    }
+    render(){
+        let cardDetails;
+        if(this.state.showDetails){
+            cardDetails = (
+                <div className="card_details">
+                    {this.props.description}
+                    <CheckList cardId={this.props.id} tasks={this.props.tasks}/>
+                </div>
+            );
+        };
+        return (
+            <div className='card'>
+                <div className="card_title" onClick={()=>this.setState({showDetails:!this.state.showDetails})
+                }>{this.props.title}</div>
+                {cardDetails}
+            </div>
+        );
+    }
+}
+
+export default Card;
