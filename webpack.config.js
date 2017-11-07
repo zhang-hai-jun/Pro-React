71877be2ad153b84a4088103165f3fff7917c269
zhang-hai-jun <790052532@qq.com>
2017-11-07 15:02:33 +0800

    第一张源码

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + "/app/app.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname,//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: __dirname,//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader"
+                    }, {
+                        loader: "postcss-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new webpack.HotModuleReplacementPlugin()//热加载插件
+    ],
+};
